8441b64e113ed3b53f57a4f5f2fe83a9fba51290
serginh023 <psergio.farias@gmail.com>
2020-06-08 20:15:31 -0300

    1.0.2 adicionado script de controle de Touch e consertado UI para telas 1080x2280

---
+++ Assets/Scripts/Managers/TouchManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchManager : MonoBehaviour
+{
+    public delegate void TouchEventHandler(Vector2 swipe);
+
+    public static event TouchEventHandler SwipeEvent;
+
+    Vector2 m_touchMovement;
+    int m_minSwipeDistance = 10;
+
+    void OnSwipe()
+    {
+        if(SwipeEvent != null)
+            SwipeEvent(m_touchMovement);
+    }
+
+    public Text mDiagnosticText1;
+    public Text mDiagnosticText2;
+
+    public bool m_useDiagnostic = false;
+
+    void Diagnostic(string text1, string text2)
+    {
+        mDiagnosticText1.gameObject.SetActive(m_useDiagnostic);
+        mDiagnosticText2.gameObject.SetActive(m_useDiagnostic);
+
+        if (mDiagnosticText1 && mDiagnosticText2)
+        {
+            mDiagnosticText1.text = text1;
+            mDiagnosticText2.text = text2;
+        }
+    }
+
+    string SwipeDiagnostic(Vector2 swipeMovement)
+    {
+        string direction = "";
+
+        if(Mathf.Abs(swipeMovement.x) > Mathf.Abs(swipeMovement.y))
+        {
+            direction = (swipeMovement.x >= 0) ? "right" : "left";
+        }
+        else
+        {
+            direction = (swipeMovement.y >= 0) ? "up" : "down";
+        }
+
+        return direction;
+    }
+
+    private void Start()
+    {
+        Diagnostic("", "");
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.touches[0];
+            if(touch.phase == TouchPhase.Began)
+            {
+                m_touchMovement = Vector2.zero;
+                Diagnostic("", "");
+            }
+            else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                m_touchMovement += touch.deltaPosition;
+
+                if(m_touchMovement.magnitude > m_minSwipeDistance)
+                {
+                    OnSwipe();
+                    Diagnostic("Swipe DetectedADASFSADFDSFSAFASFASFSAF", m_touchMovement.ToString() + " " + SwipeDiagnostic(m_touchMovement));
+                }
+            }
+
+        }
+    }
+
+}
